fab5854a95311a487fa3e0853b56f8eae358d8ae
hahrahr <hammedsaudix@gmail.com>
2023-12-08 22:06:12 +0200

    add New Activity: IntroOneActivity and program Button to Intent to SignupActivity

--- app/src/main/java/com/projecttask/zheimer/ui/IntroOneActivity.java
+++ app/src/main/java/com/projecttask/zheimer/ui/IntroOneActivity.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.Button;
 
 import com.projecttask.zheimer.R;
 import com.projecttask.zheimer.ui.auth.SignUpActivity;
 
-public class SplashActivity extends AppCompatActivity {
-
+public class IntroOneActivity extends AppCompatActivity {
+    Button introBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, IntroOneActivity.class);
+        setContentView(R.layout.activity_intro_one);
+
+        introBtn =findViewById(R.id.introBtn);
+        introBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(IntroOneActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
-        },1000);
+        });
     }
 }
