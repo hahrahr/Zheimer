215cbc22010f449ad7fd00aa9ba23e49f5f27901
hahrahr <hammedsaudix@gmail.com>
2023-12-08 21:51:42 +0200

    Add Hundler with Intent Code in SplashActivity and add logo with Text View in SplashActivity

--- app/src/main/java/com/projecttask/zheimer/ui/SplashActivity.java
+++ app/src/main/java/com/projecttask/zheimer/ui/SplashActivity.java
@@ -2,9 +2,12 @@ package com.projecttask.zheimer.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.projecttask.zheimer.R;
+import com.projecttask.zheimer.ui.auth.SignUpActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        },1000);
     }
 }
